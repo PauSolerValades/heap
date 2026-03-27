018215608f542a79dbb57bd88cdc48099a0bca8d
Pau Soler Valadés <pau.soler.valades@gmail.com>
2026-03-27 16:35:33 +0100

    forgot a name change

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const mod = b.addModule("heap", .{
+    const mod = b.addModule("ds", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
